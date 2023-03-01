cfe94736cf21a08b5efe9b95f2889a13e50e17ed
“juanP1996” <parrajp1996@gmail.com>
2023-03-01 20:41:33 +0100

    feat: agregada variables de entorno del token

--- config/auth.js
+++ config/auth.js
@@ -1,5 +1,5 @@
 module.exports = {
-    secret: process.env.AUTH_SECRET || "zA23RtfLoPP", //KEY USADA PARA ENCRIPTAR
-    expires: process.env.AUTH_EXPIRES || "24h", //DURACIÓN DEL TOKEN
-    rounds: process.env.AUTH_ROUNDS || 10 //VECES QUE SE ENCRIPTA LA CONTRASEÑA
+    secret: process.env.AUTH_SECRET , 
+    expires: process.env.AUTH_EXPIRES , 
+    rounds: process.env.AUTH_ROUNDS 
 }
